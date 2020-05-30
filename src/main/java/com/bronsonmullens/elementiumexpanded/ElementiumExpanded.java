ff32fbf2273288a7bd55481013d5ec9cd97582a1
bronsonmullens <bronsonmullens@icloud.com>
2020-05-29 22:41:08 -0700

    Set up additional supporting classes and added first item to mod

--- src/main/java/com/bronsonmullens/elementiumexpanded/ElementiumExpanded.java
+++ src/main/java/com/bronsonmullens/elementiumexpanded/ElementiumExpanded.java
@@ -8,15 +8,19 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import com.bronsonmullens.elementiumexpanded.util.RegistryHandler;
 
 @Mod("ee_bronson")
 public class ElementiumExpanded
 {
     private static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "ee_bronson";
 
     public ElementiumExpanded() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        RegistryHandler.init();
 
         MinecraftForge.EVENT_BUS.register(this);
     }
---
+++ src/main/java/com/bronsonmullens/elementiumexpanded/util/RegistryHandler.java
@@ -0,0 +1,22 @@
+package com.bronsonmullens.elementiumexpanded.util;
+
+import com.bronsonmullens.elementiumexpanded.ElementiumExpanded;
+import com.bronsonmullens.elementiumexpanded.items.ItemBase;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class RegistryHandler {
+
+    public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, ElementiumExpanded.MOD_ID);
+
+    public static void init() {
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+
+    // Items
+    public static final RegistryObject<Item> ELEMENTIUM = ITEMS.register("elementium", ItemBase::new);
+
+}
